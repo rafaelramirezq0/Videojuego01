cd833c989608046384ef6569b0ca6fd2fdf9545f
rafaelramirezq0 <66881516+rafaelramirezq0@users.noreply.github.com>
2020-06-13 12:22:20 -0500

    Add files via upload

---
+++ ga.py
@@ -0,0 +1,40 @@
+x = '"'
+'''
+for i in range(1,91):
+    print("screen.blit("+"freim"+str(i)+", [0, 0])")
+    print("time.sleep(0.04)")
+    print("pygame.display.update()")
+'''
+
+
+for i in range(418,468):
+    print("screen.blit("+"freim"+str(i)+", [0, 0])")
+    print("time.sleep(0.04)")
+    print("pygame.display.update()")
+
+'''
+
+for i in range(418,468):
+    print("freim"+str(i)+" = pygame.transform.scale(freim"+str(i)+", (800, 600))")
+
+
+
+    #screen.blit(background_image, [0, 0])
+    #time.sleep(0.01)
+    #pygame.display.update()
+
+
+
+for i in range(1,11):
+    print("freim"+str(i)+" = pygame.image.load("+x+"Animación1/intro0000"+str(i)+".png"+x+")")
+
+for i in range(11,100):
+    print("freim"+str(i)+" = pygame.image.load("+x+"Animación1/intro000"+str(i)+".png"+x+")")
+
+
+for i in range(418,468):
+    print("freim"+str(i)+" = pygame.image.load("+x+"Animación1/intro00"+str(i)+".png"+x+")")
+'''
+    #screen.blit(background_image, [0, 0])
+    #time.sleep(0.08)
+    #pygame.display.update()
